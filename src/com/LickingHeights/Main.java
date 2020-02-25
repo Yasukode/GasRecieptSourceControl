9567addca05606f7338529baa7d81e015d3ff277
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-02-25 12:07:49 -0500

    GasRecieptSourceControl

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,32 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 System.out.println("+==============================+");
+	 System.out.println("|      GEOJACKAL GAS HUB       |");
+	 System.out.println("|6969 PACKER ST.               |");
+	 System.out.println("|LOS ANGELES, CA               |");
+	 System.out.println("|                              |");
+	 System.out.println("| DATE/TIME OF PUMP USE:       |");
+	 System.out.println("|   04/11/2020  3:15PM         |");
+	 System.out.println("|                              |");
+	 System.out.println("|  PUMP 7                      |");
+	 System.out.println("| GAS: ETHANOL(CORN)85         |");
+	 System.out.println("|    GALLONS: 16               |");
+	 System.out.println("|PRICE OF GAS: $56.96US(16 GAL)|");
+	 System.out.println("|   TAX: $7.23US               |");
+	 System.out.println("| TOTAL: $64.19US              |");
+	 System.out.println("|                              |");
+	 System.out.println("| METHOD OF PAYMENT:           |");
+	 System.out.println("|    VISA: ############6232    |");
+	 System.out.println("|    VERIFICATION CODE: 3475   |");
+	 System.out.println("| AMOUNT DUE: $0.00US          |");
+	 System.out.println("|       ****************       |");
+	 System.out.println("|  THANK YOU FOR SELECTING:    |");
+	 System.out.println("|      GEOJACKAL GAS HUB!      |");
+	 System.out.println("+==============================+");
+
+
+    }
+}
